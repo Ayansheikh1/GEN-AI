67f1eab3c07bbdde7219457b6c638e469e6e73d1
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-30 15:17:44 +0530

    create database on atlads and connected to our server using mongoose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-
 require("dotenv").config();
+const app = require('./src/app');// Start the server
+const connectToDB = require('./src/config/database');
 
-const app = require('./src/app');
-// Start the server
+
+
+connectToDB();
+
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
