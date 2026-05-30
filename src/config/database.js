3cf8e1c012883ed494f5473b799ba10f6868855a
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-30 17:39:34 +0530

    comments

--- src/config/database.js
+++ src/config/database.js
@@ -10,3 +10,16 @@ async function connectToDB(){
 }
 
 module.exports = connectToDB;
+
+/*
+ * File Description:
+ * -----------------
+ * This file is responsible for establishing a connection
+ * between the application and the MongoDB database using Mongoose.
+ *
+ * Its responsibilities are:
+ * 1. Read the MongoDB connection URL from environment variables.
+ * 2. Connect the application to the MongoDB database.
+ * 3. Handle connection errors if the database is unavailable.
+ * 4. Export the connection function so it can be used during server startup.
+ */
