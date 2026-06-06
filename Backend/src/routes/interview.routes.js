7616f689a1eb441af961b446d4a6c4474882ea1d
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-06-06 13:49:33 +0530

    setup middleware and controller to routes

--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth.middleware');
+const {generateInterviewReportController} = require('../controllers/interview.controller');
+
 
 const interviewRouter = express.Router();
+
+
+/** 
+ * @routes POST /api/interview
+ * @description Generate an interview report based on the provided job description and self description.
+ * @access Private
+ *  
+ */
+
+
+interviewRouter.post('/',authMiddleware.authUser,generateInterviewReportController);
 
 module.exports = interviewRouter;
