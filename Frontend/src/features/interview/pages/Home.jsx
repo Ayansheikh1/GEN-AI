27d4350757c5138a79dfe8b633e44a9d8c4a3faf
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-06-13 16:42:09 +0530

    style home page

--- Frontend/src/features/interview/pages/Home.jsx
+++ Frontend/src/features/interview/pages/Home.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
+import '../style/home.scss'
 
 const Home = () => {
   return (
-    <main className='homee'>
-        <div className="left">
-            <textarea name="jobDescription" id="jobDescription" placeholder='Enter job description here...'></textarea>
-        </div>
-        <div className="right">
-            <div className="input-group">
-                <label htmlFor="resume">Upload Resume</label>
-                <input type="file" id="resume" name='resume' accept='.pdf' />
+    <main className='home'>
+        <div className="interview-input-group">
 
+            <div className="left">
+                <p>Job Description</p>
+                <textarea name="jobDescription" id="jobDescription" placeholder='Enter job description here...'></textarea>
             </div>
 
-            <div className="input-group">
-                <label htmlFor="selfDescription">Self Description</label>
-                <textarea name="selfDescription" id="selfDescription" placeholder='Enter self description here...'></textarea>
+            <div className="right">
+                
+                <div className="input-group">
+                    <p>Resume <small className='highlight'>(Use resume and self description together for best results)</small></p>
+                    <label className="file-label" htmlFor="resume">Upload Resume</label>
+                    <input hidden type="file" id="resume" name='resume' accept='.pdf' />
+
+                </div> 
+
+                <div className="input-group">
+                    <label htmlFor="selfDescription">Self Description</label>
+                    <textarea name="selfDescription" id="selfDescription" placeholder='Enter self description here...'></textarea>
+                </div>
+
+                <button className='button primary-button'>Generate Interview Report</button>
             </div>
 
-            <button className='generate-btn'>Generate Interview Report</button>
         </div>
     </main>
-  )
+    )
 }
 
 export default Home
