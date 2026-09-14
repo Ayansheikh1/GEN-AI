8906a87d9aae8901ff949658a6d66fcfe51b453c
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-09-14 20:16:37 +0530

    Enhance security of authentication cookies by setting httpOnly, secure, and sameSite attributes

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -45,7 +45,11 @@ async function registerUserController(req,res){
         {expiresIn:"1d"}
     );
 
-    res.cookie("token",token);
+     res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 
 
 
@@ -99,7 +103,11 @@ async function loginUserController(req,res){
         {expiresIn:"1d"}
     );
 
-    res.cookie("token",token);
+     res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 
     return res.status(200).json({
         
